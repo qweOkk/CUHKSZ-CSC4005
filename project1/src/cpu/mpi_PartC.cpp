498010cb4665d6fba3e6f0e932d5d31e36de2b33
qweokk <647038923@qq.com>
2024-09-24 07:13:33 +0000

    new

--- project1/src/cpu/mpi_PartC.cpp
+++ project1/src/cpu/mpi_PartC.cpp
@@ -15,36 +15,32 @@
 
 #define MASTER 0
 #define TAG_GATHER 0
-void set_filtered_image(JpegSOA output_jpeg, JpegSOA input_jpeg,
-                        int width, int num_chanels, int start_line,
-                        int end_line, int offset)
+
+void set_filtered_image(JpegSOA& input_jpeg,
+                        std::vector<ColorValue>& output_r_values,
+                        std::vector<ColorValue>& output_g_values,
+                        std::vector<ColorValue>& output_b_values,
+                        int width, int num_channels, int start_line,
+                        int end_line)
 {
     for (int row = start_line; row < end_line; row++)
     {
         for (int col = 1; col < width - 1; col++)
         {
-            for (int channel = 0; channel < input_jpeg.num_channels; ++channel){
-                int index = (row * width + col);
+            for (int channel = 0; channel < num_channels; ++channel)
+            {
+                int index = row * width + col;
                 ColorValue filtered_value = bilateral_filter(
-                input_jpeg.get_channel(channel), row, col, width);
-                output_jpeg.set_value(channel, index, filtered_value);
+                    input_jpeg.get_channel(channel), row, col, width);
+
+                if (channel == 0) output_r_values[index] = filtered_value;
+                if (channel == 1) output_g_values[index] = filtered_value;
+                if (channel == 2) output_b_values[index] = filtered_value;
             }
-            
-            // float r_sum =
-            //     linear_filter(image, filter, r_id, width, num_chanels);
-
-            // float g_sum =
-            //     linear_filter(image, filter, g_id, width, num_chanels);
-
-            // float b_sum =
-            //     linear_filter(image, filter, b_id, width, num_chanels);
-
-            // filtered_image[r_id - offset] = clamp_pixel_value(r_sum);
-            // filtered_image[g_id - offset] = clamp_pixel_value(g_sum);
-            // filtered_image[b_id - offset] = clamp_pixel_value(b_sum);
         }
     }
 }
+
 int main(int argc, char** argv)
 {
     if (argc != 3)
@@ -53,6 +49,7 @@ int main(int argc, char** argv)
                      "/path/to/input/jpeg /path/to/output/jpeg\n";
         return -1;
     }
+    
     // Read input JPEG File
     const char* input_filepath = argv[1];
     JpegSOA input_jpeg = read_jpeg_soa(input_filepath);
@@ -61,91 +58,87 @@ int main(int argc, char** argv)
         std::cerr << "Failed to read input JPEG image\n";
         return -1;
     }
-    // Divide the task
-    // For example, there are 11 lines and 3 tasks,
-    // we try to divide to 4 4 3 instead of 3 3 5
+
     std::cout << "Input file from: " << input_filepath << "\n";
     int width = input_jpeg.width;
     int height = input_jpeg.height;
     int num_channels = input_jpeg.num_channels;
-    ColorValue* output_r_values = new ColorValue[width * height];
-    ColorValue* output_g_values = new ColorValue[width * height];
-    ColorValue* output_b_values = new ColorValue[width * height];
+
+    // Use std::vector for output arrays to avoid manual memory management
+    std::vector<ColorValue> output_r_values(width * height);
+    std::vector<ColorValue> output_g_values(width * height);
+    std::vector<ColorValue> output_b_values(width * height);
+
     JpegSOA output_jpeg{
-        output_r_values, output_g_values, output_b_values,       width,
-        height,          num_channels,    input_jpeg.color_space};
+        output_r_values.data(), output_g_values.data(), output_b_values.data(),
+        width, height, num_channels, input_jpeg.color_space};
+
     // Start the MPI
     MPI_Init(&argc, &argv);
-    // How many processes are running
-    int numtasks;
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    // What's my rank?
-    int taskid;
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    // Which node am I running on?
-    int len;
+    int numtasks, taskid, len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Get_processor_name(hostname, &len);
     MPI_Status status;
-    int total_line_num = input_jpeg.height - 2;
+
+    int total_line_num = height - 2;
     int line_per_task = total_line_num / numtasks;
     int left_line_num = total_line_num % numtasks;
 
+    // Calculate the cut-off points for each process
     std::vector<int> cuts(numtasks + 1, 1);
-    int divided_left_line_num = 0;
     for (int i = 0; i < numtasks; i++)
     {
-        if (divided_left_line_num < left_line_num)
-        {
-            cuts[i + 1] = cuts[i] + line_per_task + 1;
-            divided_left_line_num++;
-        }
-        else
-            cuts[i + 1] = cuts[i] + line_per_task;
+        cuts[i + 1] = cuts[i] + line_per_task + (i < left_line_num ? 1 : 0);
     }
-    if (taskid == MASTER) {
+
+    if (taskid == MASTER) 
+    {
         auto start_time = std::chrono::high_resolution_clock::now();
         
-        // 主进程处理自己负责的分片
-        set_filtered_image(output_jpeg, input_jpeg, width, num_channels, cuts[taskid], cuts[taskid + 1], 0);
+        // Master process handles its own slice
+        set_filtered_image(input_jpeg, output_r_values, output_g_values, output_b_values, width, num_channels, cuts[taskid], cuts[taskid + 1]);
 
-        // 接收其他进程处理的数据
-        for (int i = 1; i < numtasks; i++) {
+        // Allocate space to receive data from other processes
+        for (int i = 1; i < numtasks; i++) 
+        {
             int start_index = cuts[i] * width;
             int length = width * (cuts[i + 1] - cuts[i]);
-            
-            // 接收每个通道的数据
-            MPI_Recv(output_r_values + start_index, length, MPI_UNSIGNED_CHAR , i, TAG_GATHER, MPI_COMM_WORLD, &status);
-            MPI_Recv(output_g_values + start_index, length, MPI_UNSIGNED_CHAR , i, TAG_GATHER, MPI_COMM_WORLD, &status);
-            MPI_Recv(output_b_values + start_index, length, MPI_UNSIGNED_CHAR , i, TAG_GATHER, MPI_COMM_WORLD, &status);
-            
+
+            // Receive filtered data for each channel from other processes
+            MPI_Recv(output_r_values.data() + start_index, length, MPI_UNSIGNED_CHAR, i, TAG_GATHER, MPI_COMM_WORLD, &status);
+            MPI_Recv(output_g_values.data() + start_index, length, MPI_UNSIGNED_CHAR, i, TAG_GATHER, MPI_COMM_WORLD, &status);
+            MPI_Recv(output_b_values.data() + start_index, length, MPI_UNSIGNED_CHAR, i, TAG_GATHER, MPI_COMM_WORLD, &status);
         }
 
         const char* output_filepath = argv[2];
         std::cout << "Output file to: " << output_filepath << "\n";
-        if (export_jpeg(output_jpeg, output_filepath)) {
+
+        // Use output_jpeg object in export_jpeg
+        if (export_jpeg(output_jpeg, output_filepath)) 
+        {
             std::cerr << "Failed to write output JPEG\n";
             return -1;
         }
 
-        delete[] output_r_values;
-        delete[] output_g_values;
-        delete[] output_b_values;
-        
         auto end_time = std::chrono::high_resolution_clock::now();
         auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
         std::cout << "Execution Time: " << elapsed_time.count() << " milliseconds\n";
-    } else {
-        // 子进程处理自己的分片
-        int length = width * (cuts[taskid + 1] - cuts[taskid]);
+
+    } 
+    else 
+    {
+        // Worker processes handle their own slice
         int offset = cuts[taskid] * width;
+        int length = width * (cuts[taskid + 1] - cuts[taskid]);
 
-        set_filtered_image(output_jpeg, input_jpeg, width, num_channels, cuts[taskid], cuts[taskid + 1], offset);
+        set_filtered_image(input_jpeg, output_r_values, output_g_values, output_b_values, width, num_channels, cuts[taskid], cuts[taskid + 1]);
 
-        // 发送处理后的数据回给主进程
-        MPI_Send(output_r_values + offset, length, MPI_UNSIGNED_CHAR , MASTER, TAG_GATHER, MPI_COMM_WORLD);
-        MPI_Send(output_g_values + offset, length, MPI_UNSIGNED_CHAR , MASTER, TAG_GATHER, MPI_COMM_WORLD);
-        MPI_Send(output_b_values + offset, length, MPI_UNSIGNED_CHAR , MASTER, TAG_GATHER, MPI_COMM_WORLD);
+        // Send filtered data back to master
+        MPI_Send(output_r_values.data() + offset, length, MPI_UNSIGNED_CHAR, MASTER, TAG_GATHER, MPI_COMM_WORLD);
+        MPI_Send(output_g_values.data() + offset, length, MPI_UNSIGNED_CHAR, MASTER, TAG_GATHER, MPI_COMM_WORLD);
+        MPI_Send(output_b_values.data() + offset, length, MPI_UNSIGNED_CHAR, MASTER, TAG_GATHER, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
